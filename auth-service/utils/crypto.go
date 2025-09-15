b92000609bae2bafc32dc0165d6e123747540086
Arif Dwi N <ardwiinoo@gmail.com>
2025-09-15 21:00:24 +0700

    Add utility functions for parsing public keys and verifying signatures

---
+++ auth-service/utils/crypto.go
@@ -0,0 +1,11 @@
+package utils
+
+import "crypto/rsa"
+
+func ParsePublicKey(pubKeyPEM string) (*rsa.PublicKey, error) {
+
+}
+
+func VerifySignature(pub *rsa.PublicKey, data, signature string) error {
+
+}
